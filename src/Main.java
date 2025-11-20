4d6d8bd13b3c18bd57bf609fc00e9fd5aaa762e8
Yeray <yeray.grajal@educa.madrid.org>
2025-11-20 09:16:09 +0100

    programa de popochos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Cuantos popochos tienes en casa?");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int n = scan.nextInt();
+
+                if (n == 0) {
+                    System.out.println("Te recomendamos obtener un popocho");
+                } else if (n == 1) {
+                    System.out.println("Este popocho esta muy mimado");
+                } else if (n == 2 || n==3) {
+                    System.out.println("La familia de popochos está equilibada");
+                } else if (n > 3) {
+                    System.out.println("Usted tiene demasiados popochos y no quedara espacio en la cama");
+                } else {
+                    System.out.println("Ese número de popochos no es válido");
+                }
+        for (int i = 1; i <= n; i++) {
+            System.out.println("El popocho " + i + " está Zzz");
         }
     }
 }
